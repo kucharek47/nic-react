3e8dc180f68121b5dba6fb6924cf36c8d6a7e7c3
Kucharek47 <matzar0@wp.pl>
2025-06-11 15:23:51 +0200

    0.3

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './css/style.css';
-import Layout from "./components/Layout";
+import Layout from "./cos/Layout";
 import Home from "./podstrony/Home";
 import NicBank from "./podstrony/NicBank";
 import NicFizyka from "./podstrony/NicFizyka";
